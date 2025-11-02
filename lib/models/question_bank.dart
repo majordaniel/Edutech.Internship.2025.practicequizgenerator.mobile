c39422612a61adeba054d6d3e7a8a3fd1d8e7d3b
Saheed Adeleye <aadesaed@gmail.com>
2025-11-02 12:50:38 +0100

    use a Map instead of a List to hold questions by course id

--- lib/models/question_bank.dart
+++ lib/models/question_bank.dart
@@ -4,32 +4,37 @@ import 'package:quiz_generator/api/api.dart' show Api;
 import 'package:quiz_generator/models/quiz.dart';
 
 class QuestionBank {
+  // TODO: store this by the courseId
   DateTime lastUpdate;
-  List<Question> _questions;
+  Map<String, List<Question>> _qMap;
   final Api _api;
 
-  QuestionBank._(this._questions, this.lastUpdate, this._api);
+  QuestionBank._(this._qMap, this.lastUpdate, this._api);
 
   factory QuestionBank.init(Api api) {
-    return QuestionBank._([], DateTime.timestamp(), api);
+    return QuestionBank._({}, DateTime.timestamp(), api);
   }
 
-  Future<List<Question>> fetchById(String courseId, int maxQuestions) async {
+  Future<List<Question>?> fetchById(String courseId, int maxQuestions) async {
     final newTimestamp = DateTime.timestamp();
     if (_shouldUpdate()) {
       final questions = await _api.fetchQuestionsFromBank(courseId);
-      _questions = questions;
       lastUpdate = newTimestamp;
+      _qMap[courseId] = questions;
     }
-    final n = min(_questions.length, maxQuestions);
-    return _questions.take(n).toList(growable: false);
+    if (_qMap.containsKey(courseId)) {
+      final q = _qMap[courseId]!;
+      final n = min(q.length, maxQuestions);
+      return q.take(n).toList(growable: false);
+    }
+    return null;
   }
 
   bool _shouldUpdate() {
     final newTimestamp = DateTime.timestamp();
     // TODO: normally the questions will be cached to a db, since that isn't ready now
     // make the question bank in-memory and only update after (ridiculous) 4 days have passed
-    return _questions.isEmpty ||
+    return _qMap.isEmpty ||
         newTimestamp.difference(lastUpdate) >= Duration(days: 4);
   }
 }
